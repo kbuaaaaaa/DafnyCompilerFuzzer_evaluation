4491a6c71e915ad5def33ba2100378cd356b5f0f
kbuaaaaaa <karnbongkot@gmail.com>
2024-08-22 18:17:37 +0100

    Echo result from interestingness test too just in case

--- scripts/interestingness_test_gen.py
+++ scripts/interestingness_test_gen.py
@@ -11,12 +11,15 @@ def generate_interestingness_test(test_folder, interpret, bug, language):
         if language != "miscompilation":
             for err in bug:
                 f.write("if ! grep -q \"{}\" fuzz-d.log; then\n".format(err))
+                f.write("    echo 1\n")
                 f.write("    exit 1\n")
                 f.write("fi\n")
         else:
             f.write("if ! grep -q \"Different output: true\" fuzz-d.log; then\n")
+            f.write("    echo 1\n")
             f.write("    exit 1\n")
             f.write("fi\n")
+        f.write("echo 0\n")
         f.write("exit 0\n")
     f.close()
     # Make the script executable
