75cbb54b9265bed84f175d381086f4a51a45770e
kbuaaaaaa <karnbongkot@gmail.com>
2024-08-29 18:34:54 +0100

    Fix interestingness test logic

--- scripts/interestingness_test_gen.py
+++ scripts/interestingness_test_gen.py
@@ -15,12 +15,12 @@ def generate_interestingness_test(test_folder, interpret, bug, language):
         
         if language != "miscompilation":
             for err in bug:
-                f.write("if ! grep -q \'{}\' fuzz-d.log; then\n".format(escape_for_grep(err)))
+                f.write("if grep -q \'{}\' fuzz-d.log; then\n".format(escape_for_grep(err)))
                 f.write("    echo 1\n")
                 f.write("    exit 1\n")
                 f.write("fi\n")
         else:
-            f.write("if ! grep -q \'{}\' fuzz-d.log; then\n".format(escape_for_grep("Different output: true")))
+            f.write("if grep -q \'{}\' fuzz-d.log; then\n".format(escape_for_grep("Different output: true")))
             f.write("    echo 1\n")
             f.write("    exit 1\n")
             f.write("fi\n")
