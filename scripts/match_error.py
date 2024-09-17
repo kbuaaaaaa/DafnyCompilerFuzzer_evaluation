7ce96f0c97ad3aa8a9ee323f3bb5d2ec8bddf02d
kbuaaaaaa <karnbongkot@gmail.com>
2024-09-17 18:09:31 +0100

    indent

--- scripts/match_error.py
+++ scripts/match_error.py
@@ -25,14 +25,16 @@ JavaScriptErrorPatterns = [ r'Error: .*\n', r'Process terminated\. .*\n', r'Unha
 ]
 GoErrorPatterns = [ r'Error: .*\n', r'Process terminated\. .*\n', r'Unhandled exception\. .+\n', r'Unhandled exception: .+\n',r'System.NotImplementedException: The method or operation is not implemented.', r'.*:\d+:\d+: .*\n', r'\[Program halted\] .*\n', r'fatal error: .*\n']
 
-known_errors = ["All elements of display must have some common supertype", "type of left argument to",
-                    "is not declared in this scope", "the type of this expression is underspecified",
-                    "branches of if-then-else have incompatible types", "the two branches of an if-then-else expression must have the same type",
-                    "incompatible types", "Microsoft.Dafny.UnsupportedInvalidOperationException", "sequence update requires the value to have the element type",
-                    "no suitable method found for", "is not iterable", "does not take any", "non-function expression",
-                    "incorrect type for selection into", "the number of left-hand sides","does not take any type arguments",
-                    "not assignable to", "cannot be applied to given types","generic array creation","expected an indented block",
-                    "Feature not supported", "implemented"]
+known_errors = [
+    "All elements of display must have some common supertype", "type of left argument to",
+    "is not declared in this scope", "the type of this expression is underspecified",
+    "branches of if-then-else have incompatible types", "the two branches of an if-then-else expression must have the same type",
+    "incompatible types", "Microsoft.Dafny.UnsupportedInvalidOperationException", "sequence update requires the value to have the element type",
+    "no suitable method found for", "is not iterable", "does not take any", "non-function expression",
+    "incorrect type for selection into", "the number of left-hand sides","does not take any type arguments",
+    "not assignable to", "cannot be applied to given types","generic array creation","expected an indented block",
+    "Feature not supported", "implemented"
+]
 
 # Map language identifiers to their respective regex patterns
 error_patterns = {
