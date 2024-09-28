a635b44949f8cce9ea6170d70083e01b36e91204
kbuaaaaaa <karnbongkot@gmail.com>
2024-09-28 15:17:59 +0100

    update some script to run 24h

--- scripts/fuzzing_entrypoint.py
+++ scripts/fuzzing_entrypoint.py
@@ -12,7 +12,7 @@ from process_bug import process_bug_handler
 # Set the commit, commit before, and duration
 branch_commit = sys.argv[1]
 main_commit = sys.argv[2]
-duration = 60*60*2
+duration = 60 * 60 * 48
 branch = sys.argv[3]
 start_time = time.time()
 
@@ -20,20 +20,15 @@ if __name__ == "__main__":
     time_interval = "30"
     repetition = os.environ.get('REPETITION')
     while (time.time() - start_time) < duration:
-        if (time.time() - start_time) > (90*60):
-            time_interval = "120"
-        elif (time.time() - start_time) > (60*60):
-            time_interval = "90"
-        elif (time.time() - start_time) > (30*60):
-            time_interval = "60"
+        time_interval = str((((time.time() - start_time) // 3600) + 1) * 60)
         # Fuzz until we hit an interesting case
         print("Fuzzing...")
         output = subprocess.run(["timeout", "180", "java", "-jar", "fuzz_d.jar", "fuzz"], capture_output=True, text=True)
         if output.returncode == 0:
             output_dir = output.stdout.split(': ')[-1].strip()
             time_passed = int(time.time() - start_time)
-            subprocess.run(["aws", "s3", "cp", f"{output_dir}/main.dfy", f"s3://compfuzzci/evaluation/all_test/{branch_commit}/{time_interval}/{repetition}/{output_dir}/main.dfy"], check=True)
-            subprocess.run(["aws", "s3", "cp", f"{output_dir}/fuzz-d.log", f"s3://compfuzzci/evaluation/all_test/{branch_commit}/{time_interval}/{repetition}/{output_dir}/{time_passed}.log"], check=True)
+            subprocess.run(["aws", "s3", "cp", f"{output_dir}/main.dfy", f"s3://compfuzzci/evaluation/24h/all_test/{branch_commit}/{time_interval}/{repetition}/{output_dir}/main.dfy"], check=True)
+            subprocess.run(["aws", "s3", "cp", f"{output_dir}/fuzz-d.log", f"s3://compfuzzci/evaluation/24h/all_test/{branch_commit}/{time_interval}/{repetition}/{output_dir}/{time_passed}.log"], check=True)
             uuid = output_dir.split('/')[-1]
             bugs = match_error(f"{output_dir}/fuzz-d.log")
             print(bugs)
